14f1baeb280544b2d3cf8bf7f97112a2153403dd
HyytJani <jhi.hyytiainen@gmail.com>
2021-09-01 07:56:55 +0300

    toka

--- main.js
+++ main.js
@@ -1,20 +1,30 @@
 
-let lomake=document.forms['syote'];
-lomake.addEventListener('submit',tulostaArvo)
 
+
+let lomake=document.forms['syote'];
+lomake.addEventListener('submit',tulostaArvo,);
 
 function tulostaArvo(event){
 
     let arvo2=document.querySelector('#arvo input[type="text"]').value;
     let valinta=document.querySelector('#yksikot').value;
-    event.preventDefault()
-
+    
+    event.preventDefault();
+   
+    let d;
+    if (document.getElementById("dec1").checked==true){
+        d=1;}
+    if (document.getElementById("dec2").checked==true){
+        d=2;}
+    if (document.getElementById("dec3").checked==true){
+        d=3;}
+    
     if(valinta==0){
-    let muunnaC= arvo2*1.8+32;
-    console.log(muunnaC);
-    console.log(valinta);}
+    let muunnosC= arvo2*1.8+32;    
+    alert(arvo2+" Celsiusta on " +muunnosC.toFixed(d)+ " Fahrenheitia");}
     
     if(valinta==1){
-        let muunnaF= (arvo2-32)/ 1.8;
-        console.log(muunnaF);
-        console.log(valinta);}}
+    let muunnosF= (arvo2-32)/ 1.8;
+        alert(arvo2+" Fahrenheitia on "+muunnosF.toFixed(d)+ " Celsiusta");}
+    }
+    
